c3a8cd5a89ded955cc7a7e917201fa381136567b
Siddharth Rama Sushil <siddharth.rohella07@gmail.com>
2026-03-07 18:59:36 -0800

    ppo_cfg

--- source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/g1_pick/agents/rsl_rl_ppo_cfg.py
+++ source/isaaclab_tasks/isaaclab_tasks/manager_based/manipulation/g1_pick/agents/rsl_rl_ppo_cfg.py
@@ -10,7 +10,7 @@ from isaaclab_rl.rsl_rl import RslRlOnPolicyRunnerCfg, RslRlPpoActorCriticCfg, R
 
 @configclass
 class G1PickPPORunnerCfg(RslRlOnPolicyRunnerCfg):
-    num_steps_per_env = 96
+    num_steps_per_env = 64
     max_iterations = 30000
     save_interval = 150
     experiment_name = "g1_pick"
@@ -18,18 +18,18 @@ class G1PickPPORunnerCfg(RslRlOnPolicyRunnerCfg):
         init_noise_std=1.0,
         actor_obs_normalization=True,
         critic_obs_normalization=True,
-        actor_hidden_dims=[256, 128, 64],  # correct for 63-dim obs
-        critic_hidden_dims=[256, 128, 64],
+        actor_hidden_dims=[512, 256, 128],  # correct for 63-dim obs
+        critic_hidden_dims=[512, 256, 128],
         activation="elu",
     )
     algorithm = RslRlPpoAlgorithmCfg(
         value_loss_coef=1.0,
         use_clipped_value_loss=True,
         clip_param=0.2,
-        entropy_coef=0.001,
+        entropy_coef=0.005,
         num_learning_epochs=5,
         num_mini_batches=4,
-        learning_rate=3.0e-4,
+        learning_rate=1.0e-3,
         schedule="adaptive",
         gamma=0.99,
         lam=0.95,
